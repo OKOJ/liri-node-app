84e141e6747492d7300ed65f8782a02ca5f5c509
OKOJ <aajmd@hotmail.com>
2019-04-05 17:51:01 -0600

    require modules

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 var axios = require('axios');
 var moment = require('moment');
+var Spotify = require('node-spotify-api');
 var command = [];
-
-
+console.log(moment());
+console.log(process.argv);
